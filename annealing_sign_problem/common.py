Fixed normalization

annealing_sign_problem/common.py
@@ -316,9 +316,12 @@ def extract_classical_ising_model(
         raise ValueError("'x' has wrong shape: {}; expected a 2D array".format(x.shape))
     # If spins come from Monte Carlo sampling, there might be duplicates.
     is_from_monte_carlo = monte_carlo_weights is not None
-    spins, counts = np.unique(spins, return_counts=True, axis=0)
-    if is_from_monte_carlo and np.any(counts != 1):
+    spins, indices, counts = np.unique(spins, return_index=True, return_counts=True, axis=0)
+    if not is_from_monte_carlo and np.any(counts != 1):
         raise ValueError("'spins' contains duplicate spin configurations")
+    if is_from_monte_carlo:
+        monte_carlo_weights = monte_carlo_weights[indices]
+        monte_carlo_weights /= np.sum(monte_carlo_weights)
 
     def forward(x: np.ndarray) -> np.ndarray:
         assert isinstance(x, np.ndarray) and x.dtype == np.uint64
@@ -352,8 +355,12 @@ def extract_classical_ising_model(
 
     if is_from_monte_carlo:
         assert np.all(monte_carlo_weights > 0)
-        monte_carlo_weights *= np.exp(-scale)
-        normalization = 1 / np.sum(monte_carlo_weights)
+        # normalization = 1 / np.sum(monte_carlo_weights)
+        # monte_carlo_weights *= np.exp(-scale)
+        # normalization = 1 / np.sum(monte_carlo_weights)
+        # weights = monte_carlo_weights / ψs ** 2
+        # normalization = np.exp(-2 * scale) / np.sum(weights)
+        normalization = 1 / np.sqrt(np.dot(counts, np.abs(ψs) ** 2 / monte_carlo_weights))
         ψs /= monte_carlo_weights
     else:
         normalization = 1 / np.linalg.norm(ψs)